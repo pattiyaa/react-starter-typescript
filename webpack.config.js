4a9ad5172a93103aad18eb811d99d8ec58e6dc54
pattiyaa <pattiyaa@gmail.com>
2017-12-28 23:15:55 +0700

    add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  context: path.join(__dirname, 'src'),
+  entry: './index.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
